154d048eca63b2b49b060b1a8590c7b3fa3022b2
ismailzebz <163643095+ismailzebz@users.noreply.github.com>
2024-06-06 19:32:10 +1200

    Making sure all comments are upto date

--- src/onlineshop/Inventory.java
+++ src/onlineshop/Inventory.java
@@ -10,17 +10,17 @@ public class Inventory {
 
     private Map<String, Product> inventory;
 
-    // this constructor initializes the private instance variables
+    //This constructor initializes the private instance variables 
     public Inventory() {
         inventory = new HashMap<>();
     }
 
-    // method for adding products into the inventory
+    // method for adding products to the inventory
     public void add(Product product) {
         inventory.put(product.get_model_name(), product);
     }
 
-    // this method returns the product on the specified index given as its parameter
+    //This method returns the product on the specified index given as its parameter
     public Product getProduct(String modelName) {
         return inventory.get(modelName);
     }
